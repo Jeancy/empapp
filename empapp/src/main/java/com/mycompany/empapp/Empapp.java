e86f16ba9682f4d810b1e4a509bb608ae2140984
Yalla Yalla Receptio <Yalla Yalla Receptio@DESKTOP-VO8PNP2>
2023-10-28 11:57:19 +0200

    Update maven pom.xml file with mysql connector dependencies.

---
+++ empapp/src/main/java/com/mycompany/empapp/Empapp.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.empapp;
+
+/**
+ *
+ * @author Yalla Yalla Receptio
+ */
+public class Empapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
